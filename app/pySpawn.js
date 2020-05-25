01b6754e1978b875f88d76feced057d179e5b23c
Sander Hellesø <sanderhelleso@hotmail.com>
2020-05-26 01:06:27 +0200

    Added updated version

--- app/pySpawn.js
+++ app/pySpawn.js
@@ -1,5 +1,30 @@
-import child_process from "child_process";
+import path from "path";
+import shell from "shelljs";
 
-const spawn = child_process.spawn;
+const scriptDir = path.join(__dirname, "../..");
 
-export default () => spawn("python", ["../../camera.py"]);
+const options = {
+  async: true,
+  silent: true
+};
+
+const commands = {
+  camera: `python ${scriptDir}/camera.py`,
+  init: `python ${scriptDir}/init.py`
+};
+
+// Recieves predicted direction from camera.py and run movement script
+const cameraPy = res => {
+  const child = exec(commands.camera);
+  child.stdout.on("data", direction => movePy(direction, res));
+};
+
+// Excecute turtlebot movement then resolves HTTP request
+const movePy = (direction, res) => {
+  const child = exec(`${commands.init} ${direction}`);
+  child.stdout.on("data", _ => res.json({ direction }));
+};
+
+const exec = cmd => shell.exec(cmd, options);
+
+export default cameraPy;
